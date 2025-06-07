edb3bd6d301321b2fdef14463861eb2085d1a795
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-06-07 20:08:04 +0300

    changed the integration url

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  // baseURL: "https://forum-backend-6h72.onrender.com/api",
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://forum-backend-6h72.onrender.com/api",
+  // baseURL: "http://localhost:5500/api",
   headers: {
     "Content-Type": "application/json",
   },
