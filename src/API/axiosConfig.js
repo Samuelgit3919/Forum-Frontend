abc383283849f9a1f5ab70241a813804ee39aa47
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-06-07 20:16:48 +0300

    changed the link of backend for front end

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "https://forum-backend-6h72.onrender.com/api",
+  baseURL: "https://forum-backend-3-w0nb.onrender.com/api",
   // baseURL: "http://localhost:5500/api",
   headers: {
     "Content-Type": "application/json",
